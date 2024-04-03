0537516263553416bccf58ba382f46dfe6b7003e
Douglas Trindade <douglastrindade22@hotmail.com>
2024-04-03 19:48:40 -0300

    feat:  scalable solution

--- compare-the-triplets.js
+++ compare-the-triplets.js
@@ -2,16 +2,14 @@ const compareTriplets = (a, b) => {
   let alicePoints = 0;
   let bobPoints = 0;
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < a.length; i++) {
     if (a[i] > b[i]) {
       alicePoints++;
     } else if (a[i] < b[i]) {
       bobPoints++;
-    } else if (a[i] === b[i]) {
-      return 0;
     }
   }
   return [alicePoints, bobPoints];
 };
 
-console.log(compareTriplets([17, 28, 30], [99, 16, 8]));
+console.log(compareTriplets([17, 28, 30, 4], [99, 16, 8, 10]));
