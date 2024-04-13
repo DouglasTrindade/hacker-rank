caed565e4e5cc819ccc0e737ae187dc7a69f8231
Douglas Trindade <douglastrindade22@hotmail.com>
2024-04-12 22:38:30 -0300

    feat: breaking records

---
+++ breaking-the-records.js
@@ -0,0 +1,22 @@
+const breakingRecords = (scores) => {
+  let max = scores[0];
+  let min = scores[0];
+  let countMax = 0;
+  let countMin = 0;
+
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] > max) {
+      max = scores[i];
+      countMax++;
+    }
+
+    if (scores[i] < min) {
+      min = scores[i];
+      countMin++;
+    }
+  }
+
+  return [countMax, countMin];
+};
+
+console.log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]));
